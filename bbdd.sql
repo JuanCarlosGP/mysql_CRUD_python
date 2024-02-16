eb9aa70ee04170afd744a83f66cf044375aa5606
JuanCarlosGP <juancarlosgp212@gmail.com>
2024-02-16 18:13:53 +0100

    Files

---
+++ bbdd.sql
@@ -0,0 +1,13 @@
+CREATE DATABASE IF NOT EXISTS empleados;
+
+USE empleados;
+
+CREATE TABLE `mae_empleados` (
+    `codigo` VARCHAR(4) NOT NULL,
+    `nombre` VARCHAR(30) NOT NULL,
+    `apellidos` VARCHAR(45) NOT NULL,
+    `nif` VARCHAR(8) NOT NULL,
+    `departamento` VARCHAR(15) NOT NULL,
+    `num_hijos` INT NOT NULL,
+    PRIMARY KEY(`codigo`)
+    );
